3921f6f6ae8443bcd40106a4b575f1476d93f116
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-29 03:39:50 +0000

    Automatically backed up by Learn

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Home from './Home'
+import About from './About'
+import Signup from './Signup'
+import Login from './Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
